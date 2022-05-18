4f054dfa618bbaccc5f39c62d7124ecf076e05c6
Simon Cromwell <cromwellsimon@gmail.com>
2022-05-17 22:52:13 -0400

    Test change to see if making a new branch will resolve my issue of the web deployment deleting everything on Main lol

--- Godot/Calculator/Output/Output.gd
+++ Godot/Calculator/Output/Output.gd
@@ -4,8 +4,8 @@ export(NodePath) var output_text_label_path
 onready var output_text_label = get_node(output_text_label_path)
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+#func _ready():
+#	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
